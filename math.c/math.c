0297d2866838624ce872279f94bc8c7980544536
anisa9292 <anisarohilath@gmail.com>
2019-03-03 12:11:54 +0530

    commit

---
+++ math.c/math.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+//this is a sin function
+void sin()
+{
+   printf("we are calling sin function");
+}
+//this is cos function
+void cos()
+{
+   printf("we are calling cos function");
+}
+void tan()
+{
+   printf("we are calling tan function");
+}
+void main()
+{
+  sin();
+  cos();
+  tan();
+}
